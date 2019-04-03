430ab81a3beb053e11a3545804b371f45a9fc980
olektomek <olekstomek@gmail.com>
2019-04-03 20:59:28 +0200

    propozycja rozwiązania

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String text = scanner.nextLine();
+
+        System.out.format(text);
+    }
+}
