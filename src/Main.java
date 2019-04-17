540791ad61fed1886b9724324c7c8a0c9c793421
olektomek <olekstomek@gmail.com>
2019-04-17 23:52:58 +0200

    docelowe rozwiązanie - prawidłowe

--- src/Main.java
+++ src/Main.java
@@ -6,5 +6,8 @@ public class Main {
         String text = scanner.nextLine();
 
         System.out.format("%s", text);
+
+        // docelowe rozwiązanie
+        System.out.\u0070\u0072\u0069\u006e\u0074\u006c\u006e("\n" + text);
     }
 }
